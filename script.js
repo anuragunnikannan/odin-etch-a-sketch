b944a6628a1238f2f58155be8f129a9d6c901193
Anurag Unnikannan <anuragunnikannan7@gmail.com>
2023-04-03 17:31:48 +0530

    Added draw by dragging

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 let mode = "normal";
-let clicked = false;
-function changeMode(e)
-{
+
+function changeMode(e) {
     mode = e.id;
 }
 
-function changeColour(e)
-{
-    if(clicked)
-    {
-        if(mode === "normal")
-        {
-            let colour = document.querySelector("#colourpicker").value;
-            e.target.style.backgroundColor = colour;
-        }
-        else if(mode === "rainbow")
-        {
-            let red = Math.floor((Math.random() * 255));
-            let green = Math.floor((Math.random() * 255));
-            let blue = Math.floor((Math.random() * 255));
-            e.target.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
-        }
-        else if(mode == "eraser")
-        {
-            e.target.style.backgroundColor = "white";
-        }
+function changeColour(e) {
+    if (mode === "normal") {
+        let colour = document.querySelector("#colourpicker").value;
+        e.target.style.backgroundColor = colour;
+    }
+    else if (mode === "rainbow") {
+        let red = Math.floor((Math.random() * 255));
+        let green = Math.floor((Math.random() * 255));
+        let blue = Math.floor((Math.random() * 255));
+        e.target.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
+    }
+    else if (mode == "eraser") {
+        e.target.style.backgroundColor = "white";
     }
 }
 
@@ -34,23 +26,21 @@ function clearGrid()
     x.forEach(y => y.style.backgroundColor = "white");
 }
 
-function generateGrid(size)
-{
-    document.getElementById("selection").innerText = ""+size+"x"+size;
-    let container = document.getElementById("container");
-    container.innerHTML = "";
+function generateGrid(size) {
+    document.querySelector("#selection").innerText = `${size} x ${size}`;
+    let container = document.querySelector("#container");
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for(let c = 0; c < (size * size); c++)
-    {
+    for (let c = 0; c < size ** 2; c++) {
         let cell = document.createElement("div");
-        cell.className = "grid-item";
-        cell.addEventListener("mouseover", changeColour);
-        cell.addEventListener("mousedown", changeColour);
+        cell.classList.add("grid-item");
+        cell.addEventListener("mousedown", (e) => changeColour(e));
+        cell.addEventListener("mouseover", (e) => {
+            if (e.buttons == 1)
+                changeColour(e);
+        });
         container.appendChild(cell);
     }
 }
-generateGrid(document.getElementById("size").value);
 
-document.querySelector("#container").addEventListener("click", () => clicked = !clicked);
-document.querySelectorAll("button").forEach(button => button.addEventListener("click", () => clicked = false));
+generateGrid(document.querySelector("#size").value);
